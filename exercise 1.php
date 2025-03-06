372ab5f31dc32878dab13153ec5d240f021d49d9
M303User <M303User@m303.uc-bcf.edu.ph>
2025-03-06 15:55:02 +0800

    exercise 1

---
+++ exercise 1.php
@@ -0,0 +1,14 @@
+<?php
+$a = 15;
+$b = 7;
+
+$sum = $a + $b;
+$difference = $a + $b;
+$product = $a + $b;
+$quotient = $a + $b;
+
+echo "Sum: " . ($a + $b) . "\n";
+echo "Difference: " . ($a - $b) . "\n";
+echo "Product: " . ($a * $b) . "\n";
+echo "Quotient: " . ($a / $b) . "\n";
+?>
